501c516c621454b16379f2b7ba41b067dd12f632
amshelhack3r <amshelhack3r@gmail.com>
2018-11-28 23:17:05 +0300

    initial commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.app')
+@section('content')
+<h1>ABOUT US PAGE</h1>
+@endsection
---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+<nav class="navbar-main">
+    <section class="container">
+        <a class="company-title" href="#">Mutall Devs</a>
+        <ul class="nav-list">
+            <li class="nav-list-item"><a class="list-item-link" href="#">ASK</a></li>
+            <li class="nav-list-item"><a class="list-item-link" href="#">POST</a></li>
+        </ul>
+        <a href="">REPO</a>
+    </section>
+
+</nav>
