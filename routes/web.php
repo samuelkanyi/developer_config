0c1f3cf61227197bb361109652e29f71c6a899c1
amshelhack3r <amshelhack3r@gmail.com>
2018-11-29 00:55:08 +0300

    routes now go to the controller

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', 'PagesController@index');
 
-Route::get('/about', function () {
-    return view('about');   
-});
+Route::get('/about', 'PagesController@about');
