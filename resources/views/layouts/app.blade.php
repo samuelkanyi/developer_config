9a5abb200d53275b1f6f4eea6323d3304027884e
amshelhack3r <amshelhack3r@gmail.com>
2018-12-07 10:35:24 +0300

    main blade layout file

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,19 +1,24 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{config('app.name')}}</title>
+        <title>{{ config('app.name', 'Mutall Devs') }}</title>
+
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
 
         <!-- Fonts -->
-        <link href="{{asset('css/app.css')}}" rel="stylesheet" >-->
-        <!--<link rel="stylesheet" href="https://unpkg.com/picnic">-->
+        <link rel="dns-prefetch" href="//fonts.gstatic.com">
+       
         <!-- Styles -->
-
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
     <body>
-        @yield('nav')
+        @include('layouts.nav')
         <div class="content container">
             @yield('content')
         </div>
