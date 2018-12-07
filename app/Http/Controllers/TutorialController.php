4366654352b903dda1924251daa68d217540f2d3
amshelhack3r <amshelhack3r@gmail.com>
2018-12-07 10:20:43 +0300

    add crud functionality to tutorialcontroller

---
+++ app/Http/Controllers/TutorialController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Tutorial;
+class TutorialController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $tutorials = Tutorial::all();
+        return view('tutorial.index')->with('tutorials', $tutorials);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+    
+        return view('tutorial.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $tutorial = new Tutorial;
+        $tutorial->title = $request->input('title');
+        $tutorial->body= $request->input('body');
+        $tutorial->user = auth()->user()->id;
+        $tutorial->save();
+        return redirect('tutorial');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $tutorial = Tutorial::find($id);
+        return view('tutorial.show')->with('tutorial', $tutorial);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
