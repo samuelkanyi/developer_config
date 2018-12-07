7ba73c94d3df4004216bbc4d6e454fc910ee4bdc
amshelhack3r <amshelhack3r@gmail.com>
2018-12-07 10:35:07 +0300

    blade layout for main page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+@section('content')
+<p>View our list of curated tutorials to help working with mutall tools</p>
+<a class="btn" href="/tutorial">View Tutorials</a>
+@endsection
