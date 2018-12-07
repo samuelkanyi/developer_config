6fef00eb09e3bf01876c447688cb81cbe386cb5a
amshelhack3r <amshelhack3r@gmail.com>
2018-12-07 10:25:03 +0300

    add migrations for different models

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,16 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name', 30);
+            $table->string('email', 50)->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password', 100);
+            $table->string("position", 30);
+            $table->string("skill", 30);
+            $table->string("avatar", 30)->unique();
             $table->rememberToken();
             $table->timestamps();
+            
         });
     }
 
--- database/migrations/2018_12_06_190837_add_user_to_related_tables.php
+++ database/migrations/2018_12_06_190837_add_user_to_related_tables.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePasswordResetsTable extends Migration
+class AddUserToRelatedTables extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,8 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email', 50)->index();
-            $table->string('token', 100);
-            $table->timestamp('created_at')->nullable();
+        Schema::table('tutorials', function (Blueprint $table) {
+            $table->integer("users");
         });
     }
 
@@ -27,6 +25,8 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::table('tutorials', function (Blueprint $table) {
+            $table->dropColumn("users");
+        });
     }
 }
