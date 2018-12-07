7d78872d39a47a114f1cf08ee10dbd176c79ebc5
amshelhack3r <amshelhack3r@gmail.com>
2018-12-07 10:37:02 +0300

    show all tutorials

--- resources/views/tutorial/index.blade.php
+++ resources/views/tutorial/index.blade.php
@@ -1,70 +1,29 @@
-@extends('layouts.nav')
+@extends('layouts.app')
 @section('content')
-<input id="search" type="text" placeholder="Search Process"/>
+<input id="search" type="text" placeholder="Search Tutorial"/>
 
 <section class="main">
-    <div class="process">
-        <div class="process-image">
-            <img src="{{asset('images/logo.png')}}" alt=""/>
-        </div>
-        <div class="process-body">
-            <div class="process-title">
-                <h3>Install and configure Laravel<span class="user">By amshel</span></h3>
+    @if(count($tutorials)>0)
+        @foreach($tutorials as $tutorial)
+            <div class="process">
+                <div class="process-image">
+                    <img src="{{asset('images/logo.png')}}" alt=""/>
+                </div>
+                <div class="process-body">
+                    <div class="process-title">
+                        <h3>{{$tutorial->title}}</h3>
+                        <span class="user">Created at {{$tutorial->created_at}}</span>
+                    </div>
+                    <div class="process-detail">
+                        {!! str_limit($tutorial->body, 300, '...')!!}
+                        <a href="/tutorial/{{$tutorial->id}}">Read more</a>
+                    </div>
+                </div>
             </div>
-            <div class="process-detail">
-                Lorem ipsum dolor sit amet, 
-                consectetur adipiscing elit. 
-                Quisque sapien velit, aliquet 
-                eget commodo nec, auctor a 
-                sapien. Nam eu neque 
-                vulputate diam rhoncus 
-                faucibus. Curabitur quis 
-                varius libero. Lorem... <a class="button" href="">Read More</a>
-            </div>
-        </div>
-    </div>
-    
-    
-    <div class="process">
-        <div class="process-image">
-            <img src="{{asset('images/etl.jpeg')}}" alt=""/>
-        </div>
-        <div class="process-body">
-            <div class="process-title">
-                <h3>Configuring Mutall Studio<span class="user">By Muraya</span></h3>
-            </div>
-            <div class="process-detail">
-                Lorem ipsum dolor sit amet, 
-                consectetur adipiscing elit. 
-                Quisque sapien velit, aliquet 
-                eget commodo nec, auctor a 
-                sapien. Nam eu neque 
-                vulputate diam rhoncus 
-                faucibus. Curabitur quis 
-                varius libero. Lorem... <a class="button" href="">Read More</a>
-            </div>
-        </div>
-    </div>
-    
-        <div class="process">
-        <div class="process-image">
-            <img src="{{asset('images/netbeans.png')}}" alt=""/>
-        </div>
-        <div class="process-body">
-            <div class="process-title">
-                <h3>Connecting netbeans to remote server<span class="user">By Samuel</span></h3>
-            </div>
-            <div class="process-detail">
-                Lorem ipsum dolor sit amet, 
-                consectetur adipiscing elit. 
-                Quisque sapien velit, aliquet 
-                eget commodo nec, auctor a 
-                sapien. Nam eu neque 
-                vulputate diam rhoncus 
-                faucibus. Curabitur quis 
-                varius libero. Lorem... <a class="button" href="">Read More</a>
-            </div>
-        </div>
-    </div>
- </section>
+        <hr>
+        @endforeach
+    @else
+        <h2>No Tutorials Found</h2>
+    @endif
+</section>
 @endsection
