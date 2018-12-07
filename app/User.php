d5d6d3ca8217ee2a5eab54c5592e1294f35e92c6
amshelhack3r <amshelhack3r@gmail.com>
2018-12-07 10:23:16 +0300

    add position, skill and avatar to fillable field

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'position', 'skill', 'avatar'
     ];
 
     /**
@@ -27,4 +27,8 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+    
+    public function tutorial(){
+        return $this->hasMany("App\Tutorial", "users");
+    }
 }
