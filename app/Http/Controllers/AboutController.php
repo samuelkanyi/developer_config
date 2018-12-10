0698fa3bdb9004f326ee987797ceb62b38178a4c
amshelhack3r <amshelhack3r@gmail.com>
2018-12-10 16:15:40 +0300

    add controller for the about page

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+class AboutController extends Controller
+{
+    public function index(){
+        $users = User::all(); 
+        return view('about')->with('users', $users);
+    }
+}
