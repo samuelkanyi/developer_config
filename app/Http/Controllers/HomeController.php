17e83ddd27414961a93bf5f38ebcb5535029c8d2
amshelhack3r <amshelhack3r@gmail.com>
2018-12-10 16:16:50 +0300

    return view with tutorials for that user

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class HomeController extends Controller
 {
@@ -23,7 +24,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $tutorials = User::find(auth()->user()->id)->tutorial;
+        return view('home')->with('tutorials', $tutorials);
     }
 
 }
