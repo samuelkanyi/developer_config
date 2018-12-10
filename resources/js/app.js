7539a9a412d5433be8403760c6e487689c80a7c5
amshelhack3r <amshelhack3r@gmail.com>
2018-12-10 16:24:53 +0300

    compiled javascript

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,33 +1,36 @@
+//get the modal
+const modal = document.querySelector(".modal");
+//get the anchor tag used to open the modal
+const anchors = document.querySelectorAll(".anchor-delete");
 
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+for (let anchor of anchors) {
+    anchor.onclick = (e) => {
+        e.preventDefault();
+        modal.style.display = 'block';
+    };
+}
 
-require('./bootstrap');
+//get buttons for cancelling
+const cancel = document.querySelectorAll(".btn-cancel");
+for (let cancel_btn of cancel){
+    cancel_btn.onclick = (e)=>{
+        modal.style.display = 'none';
+    }
+}
 
-window.Vue = require('vue');
+//close the modal when user clicks anywhere else
+window.onclick =(event)=>{
+    console.log(event.target);
+    if(event.target === modal){
+        modal.style.display = 'none';
+    }
+};
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
-
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
-
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key)))
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-const app = new Vue({
-    el: '#app'
-});
+//change the profile picture button 
+const prof_change = document.querySelector(".prof_change");
+//get the form class name
+const form = document.querySelector(".change-profile");
+prof_change.onclick =()=>{
+    form.style.display = 'block';
+    prof_change.style.display = 'none';
+};
